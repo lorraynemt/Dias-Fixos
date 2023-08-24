6bd9b54d026504381221fb8f1d2d63a1e1b1289f
Lorrayne <lorrayne.traspadini@gmail.com>
2023-08-24 13:24:57 -0300

    ex das aulas 3 e 4

--- tipos-de-variaveis/src/TiposVariaveis.java
+++ tipos-de-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+
+        final double VALOR_DE_PI = 3.15;
+        System.out.print(VALOR_DE_PI);
+
     }
 }
