121e6209208e660db8d0e3026f8f570d41b27cd6
Lorrayne <lorrayne.traspadini@gmail.com>
2023-08-25 14:02:38 -0300

    ex 2 op arit, unit, terna

--- tipos-de-variaveis/src/Operadores.java
+++ tipos-de-variaveis/src/Operadores.java
@@ -1,6 +1,6 @@
 public class Operadores {
     public static void main(String[] args) throws Exception {
-        String nomeCompleto = "LINGUAGEM " + "JAVA";
+        /*String nomeCompleto = "LINGUAGEM " + "JAVA";
         System.out.print(nomeCompleto);
 
         String concatenacao = "? ";
@@ -15,5 +15,32 @@ public class Operadores {
         System.out.print(concatenacao);
         concatenacao = " 1" + (1 + 1 + 1);
         System.out.print(concatenacao);
+
+        int numero = 5;
+        System.out.println(- numero);
+
+        numero ++;
+        System.out.println(numero);
+
+        boolean verdadeiro = true;
+        System.out.print(verdadeiro);
+
+        System.out.print("Inverteu " + !verdadeiro);*/
+
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String resultado;
+        if (a == b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+        
+        System.out.println(resultado);
+
+        String resultado2 = (a == b) ? "verdadeiro" : "falso";
+        System.out.println(resultado2);
+
     }
 }
